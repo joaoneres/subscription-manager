619ae89297e573effd7be67a87869a15da3b5e91
João Vitor <leof.neres@gmail.com>
2022-06-14 16:09:56 -0300

    UserRegisterRequest implements

---
+++ app/Http/Requests/UserRegisterRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\CpfCnpjRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRegisterRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'document' => ['required', 'string', new CpfCnpjRule()],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phone' => ['required', 'string', 'regex:/^\(?(?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ];
+    }
+}
