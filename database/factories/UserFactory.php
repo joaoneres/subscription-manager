21d38933a5c176aa62bd2b73446fb38c4aac08bb
João Vitor <leof.neres@gmail.com>
2022-06-14 16:02:57 -0300

    user factory updates to consider new table columns and admin user type

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,21 +18,29 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'phone' => $this->faker->regexify('55[0-9]{11}'),
+            'phone_verified_at' => now(),
+            'document' => $this->faker->regexify('[0-9]{11}'),
+            'is_admin' => false,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
     public function unverified()
     {
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
+            ];
+        });
+    }
+
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'is_admin' => true,
             ];
         });
     }
