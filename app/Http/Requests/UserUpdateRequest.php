913447c9aab24cff96e25d01cde768482ef2b11c
João Vitor <leof.neres@gmail.com>
2022-06-14 23:41:15 -0300

    UserUpdateRequest

---
+++ app/Http/Requests/UserUpdateRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\CpfCnpjRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserUpdateRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return auth()->check();
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => ['nullable', 'string', 'max:255'],
+            'document' => ['nullable', 'string', new CpfCnpjRule(), 'unique:users,document,'.request()->query('user')],
+            'email' => ['nullable', 'string', 'email', 'max:255', 'unique:users'],
+            'phone' => ['nullable', 'string', 'regex:/^\(?(?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/'],
+        ];
+    }
+}
