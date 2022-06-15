b4d9022e0419177b85caf8bf132fccffb9db21f0
João Vitor <leof.neres@gmail.com>
2022-06-14 23:39:25 -0300

    show view to users manegement

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('customCss')
+    <style>
+        th, td {
+            vertical-align: middle;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <div class="container">
+        <h1>See User</h1>
+        <h3>{{ $user->name }}</h3>
+        <a href="{{ route('users.index') }}" class="btn btn-primary mb-3">Back</a>
+
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">E-mail</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Document</th>
+                        <th scope="col">Created at</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->phone }}</td>
+                        <td>{{ $user->document }}</td>
+                        <td>{{ $user->created_at->diffForHumans(now()) }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
