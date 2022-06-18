dfeca0a625d3dbf91f4ef126415cf092d863b1db
João Vitor <leof.neres@gmail.com>
2022-06-18 17:47:17 -0300

    routes updates to consider profile endpoints

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,9 @@ Auth::routes(['verify' => true]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('users', App\Http\Controllers\UserController::class)->except('store', 'create')->middleware('auth');
+
+Route::prefix('profile')->group(function() {
+    Route::get('/', [App\Http\Controllers\ProfileController::class, 'profile'])->name('profile');
+    Route::post('{user}/avatar', [App\Http\Controllers\ProfileController::class, 'updateAvatar'])->name('profile.avatar');
+    Route::post('{user}/simple-data', [App\Http\Controllers\ProfileController::class, 'simpleData'])->name('profile.simple-data');
+});
