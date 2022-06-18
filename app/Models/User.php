e665f8ff52a55b30b5bd7bb0640834821cf65c90
João Vitor <leof.neres@gmail.com>
2022-06-18 17:50:14 -0300

    user model avatar relation update

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,32 +15,22 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name', 'email', 'password', 'document', 'is_admin', 'phone',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function avatar()
+    {
+        return $this->morphOne('App\Models\Image', 'imageable');
+    }
 
     public function sendEmailVerificationNotification()
     {
