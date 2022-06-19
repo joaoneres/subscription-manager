7c1125e9c4edda5eb6bfeb5a302a59e945338766
João Vitor <leof.neres@gmail.com>
2022-06-19 00:53:58 -0300

    user update request adaptations

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -16,8 +16,8 @@ class UserUpdateRequest extends FormRequest
     {
         return [
             'name' => ['nullable', 'string', 'min:5', 'max:150'],
-            'document' => ['nullable', 'string', new CpfCnpjRule(), 'unique:users,document,'.request()->query('user')],
-            'email' => ['nullable', 'string', 'email', 'min:5', 'max:255', 'unique:users'],
+            'document' => ['nullable', 'string', new CpfCnpjRule()],
+            'email' => ['nullable', 'string', 'email', 'min:5', 'max:255'],
             'phone' => ['nullable', 'string', 'regex:/^\(?(?:[14689][1-9]|2[12478]|3[1234578]|5[1345]|7[134579])\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/'],
             'is_admin' => ['nullable', 'boolean'],
         ];
