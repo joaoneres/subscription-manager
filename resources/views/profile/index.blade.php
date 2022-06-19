a6afef59035d36c6c481d2ce0b15d7c047301477
João Vitor <leof.neres@gmail.com>
2022-06-19 00:54:33 -0300

    profile view lang adaptations

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -27,7 +27,7 @@
                 </div>
                 @endif
 
-                <form method="POST" action="{{ route('profile.simple-data', ['user' => auth()->user()->id + 2]) }}"
+                <form method="POST" action="{{ route('profile.simple-data', ['user' => auth()->user()->id]) }}"
                     enctype="multipart/form-data">
                     @csrf
 
