d0b4d4bc10bbaf26629b6238429014a832883aa8
João Vitor <leof.neres@gmail.com>
2022-06-19 14:10:19 -0300

    user sendPasswordResetNotification locale adaptationh

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\App;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
@@ -40,6 +41,6 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function sendPasswordResetNotification($token)
     {
-        $this->notify(new UserResetPasswordNotification($token));
+        $this->notify(new UserResetPasswordNotification($token, App::getLocale()));
     }
 }
