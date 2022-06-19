226875727e6b91e279e4943edd77d57af4882889
João Vitor <leof.neres@gmail.com>
2022-06-19 00:13:04 -0300

    user model locale field fillable property

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     protected $fillable = [
         'name', 'email', 'password', 'document', 'is_admin', 'phone',
+        'locale',
     ];
 
     protected $hidden = [
