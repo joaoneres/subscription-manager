62053ca1d523c38b0d5e61341ded4f1d54ccc946
João Vitor <leof.neres@gmail.com>
2022-06-22 13:54:46 -0300

    app service provider refactor to consider input component adaptations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,9 @@
 
 namespace App\Providers;
 
-use App\View\Components\EmailInput;
-use App\View\Components\PasswordInput;
+use App\View\Components\Input;
 use App\View\Components\SelectInput;
 use App\View\Components\SingleError;
-use App\View\Components\TextInput;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -29,10 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::component(TextInput::class, 'text-input');
+        Blade::component(Input::class, 'input');
         Blade::component(SingleError::class, 'single-error');
         Blade::component(SelectInput::class, 'select-input');
-        Blade::component(PasswordInput::class, 'password-input');
-        Blade::component(EmailInput::class, 'email-input');
     }
 }
