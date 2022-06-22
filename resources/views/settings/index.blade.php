e366af6fc79af63ba48d2423c43b975c692b3970
João Vitor <leof.neres@gmail.com>
2022-06-22 13:47:32 -0300

    settings index blade updates on select input component

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -13,7 +13,7 @@
                         <form action="{{ route('settings.locale') }}" method="POST">
                             @csrf
 
-                            <x-select-input :col="'12'" :selected="auth()->user()->locale" :options="App\Enums\LocaleEnum::toSelectInput()" :field="'locale'" :required="true" :mb="'3'" />
+                            <x-select-input :col="'12'" :selected="auth()->user()->locale" :options="App\Enums\LocaleEnum::toSelectInput()" :field="'locale'" :mb="'3'" />
 
                             <button type="submit" class="btn btn-primary">{{ __('Update') }}</button>
                         </form>
